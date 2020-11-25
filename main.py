f78bbb12fe708418e7b36f5ec7ef0289bc9beb60
6y6lek <49441978+6y6lek@users.noreply.github.com>
2020-11-25 13:35:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import os
-import pandas as pd
 from google.cloud import translate_v2 as translate
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] =r"D:\Python\my-translation-api-7f9d257e6b35.json"
 
 translate_client = translate.Client()
 
-languages=translate_client.get_languages()
 
-languagelist = pd.DataFrame(languages)
-
-print(languagelist)
 
 text = open('input.txt', 'r', encoding="utf8").read()
 
